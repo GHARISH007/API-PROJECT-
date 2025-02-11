a9746fbfd4cb2f8b7a304d5b82689ffc71520b67
Hari-doss4 <haribalu1003@gmail.com>
2025-02-11 10:55:25 +0530

    Add project files.

---
+++ crud50/Controllers/DemoApiController.cs
@@ -0,0 +1,25 @@
+﻿using crud50.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud50.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoApiController : ControllerBase
+    {
+        private readonly ApiDemoDbContext _apiDemoDbContext;
+        public DemoApiController(ApiDemoDbContext apiDemoDbContext)
+        {
+            _apiDemoDbContext = apiDemoDbContext;
+        }
+        [HttpGet]
+        [Route("get-user-List")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var users=await _apiDemoDbContext.User.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
---
+++ crud50/models/ApiDemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace crud50.models
+{
+    public class ApiDemoDbContext:DbContext
+    {
+        public ApiDemoDbContext(DbContextOptions<ApiDemoDbContext> options) : base(options)
+        { }
+        public DbSet<User> User { get; set; }
+    }
+}
+   
